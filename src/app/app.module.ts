0feeae62d842c13375aa7d01c8c3903d267426da
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2022-04-08 16:40:45 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './components/title/title.component';
+import { AboutComponent } from './sections/about/about.component';
+
+import { HighlightJsModule } from 'ngx-highlight-js';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TitleComponent, AboutComponent],
+  imports: [BrowserModule, HighlightJsModule ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
